6e82ffeff77e6fbf26a17f2bd998ec23d748160e
Alvin <a.mudaheran@alustudent.com>
2026-06-04 13:30:28 +0200

    Script moves files

---
+++ hospital_archive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Where active logs are stored
+ACTIVE_DIR="active_logs"
+
+# Where archived logs are moved
+ARCHIVE_DIR="archived_logs"
+
+# Creates the current date and time for each file
+TIMESTAMP=$(date +"%Y%m%d_%H%M")
+
+
+mkdir -p "$ARCHIVE_DIR"
+
+for log_file in "$ACTIVE_DIR"/*.log; do
+ base_name=$(basename "$log_file" .log)
+
+# Moves the log files to archived_logs renamed with timestamps
+mv "$log_file" "$ARCHIVE_DIR/${base_name}_${TIMESTAMP}.log"
+
+# Recreates a fresh empty log file in active_logs
+touch "$log_file"
+
+done
